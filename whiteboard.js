e5599902a08efdddf8bbd63d5cc10358e205ce9a
Luiza Santiago <lu.sntgo@gmail.com>
2022-06-06 16:01:50 -0300

    :hammer: add whiteboard

--- whiteboard.js
+++ whiteboard.js
@@ -1,36 +1,48 @@
-
 //! COMPLEXIDADE O(N)^
-function encontraSoma(lista, soma){
-    for (let i = 0; i < lista.length; i++) {
-        const valor1 = lista[i];
-        const complemento = soma - valor1;
-        const valor2 = buscaBinaria(lista.slice(i+1), complemento);
+function encontrarSoma(lista, soma) {
+  for (let i = 0; i < lista.length; i++) {
+    const valor1 = lista[i];
+    const complemento = soma - valor1;
+    const valor2 = buscaBinaria(lista.slice(i + 1), complemento);
 
-        if (valor2 === null) return true;
-    }
-    return false
+    if (valor2 === null) return true;
+  }
+  return false;
 }
 
 //! COMPLEXIDADE O(N)
 
+function encontrarSoma(list) {
+  // list [1234, 1234, 4321, 5432, ...]
+  const hash = {};
+  const repete = [];
 
-function duplicados(list){  // list [1234, 1234, 4321, 5432, ...]
-    const hash = {}
-    const repete = []
-
-    for(let i = 0; i < list.length; i++){
-        const item = list[i]
-        if(hash[item]){
-            hash[item] = true // hash {1234: true}
-        } else {
-            hash[item] = false // hash {4321: false, 5432: false}
-        }
-    }    
-    for(let key in hash){
-        const value = hash[key]
-        if (value === true){
-        repete.push(key)
-        }
+  for (let i = 0; i < list.length; i++) {
+    const item = list[i];
+    if (hash[item]) {
+      hash[item] = true; // hash {1234: true}
+    } else {
+      hash[item] = false; // hash {4321: false, 5432: false}
     }
-    return repete
+  }
+  for (let key in hash) {
+    const value = hash[key];
+    if (value === true) {
+      repete.push(key);
+    }
+  }
+  return repete;
+}
+
+function encontrarIndice(lst){
+    const hash = {}
+
+    for (let i = 0; i < lst.length; i++){
+        const item = lst[i]
+        if(lst[hash[item]] === lst[i]){
+            return [hash[item], i]
+        }
+        hash[item] = i  
+    }
+    return false
 }
